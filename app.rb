01881548fbbb87e779fc974bbb59cc3d813fb895
Rachel Grier <rgrier104@gmail.com>
2019-08-14 02:43:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,4 +32,12 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:numbr1].to_i
+    @number2 = params [:number2].to_i
+    if @operations == "add"
+      "#{@number1 + @number2}"
+  end
+
 end
