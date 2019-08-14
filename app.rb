acad8ae40b868593c189e81683ce54117a898315
Rachel Grier <rgrier104@gmail.com>
2019-08-14 02:36:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
     n=0
     while n < @number
